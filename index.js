41f95ed27f7248d418c1c1af99a0460b8e2a5824
Miroslav Gurnyak <mirmiroslav@gmail.com>
2024-08-31 19:14:09 +0300

    Wordborne

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const btn = document.querySelector(".btn");
+const genBtn = document.querySelector(".gen-btn");
 const display = document.querySelector(".display");
 const numOfLetters = document.querySelector(".letters-num");
+const saveBtn = document.querySelector(".save-btn");
 
-const wovs = ["A", "E", "I", "O", "U", "Y"];
+const delIcon = `<img src="delete.svg" width="15">`;
+const saveIcon = `<img src="save.svg" width="20">`;
+
+saveBtn.innerHTML = saveIcon;
+
+const wovs = ["A", "E", "I", "O", "U"];
 
 const cons = [
   "B",
@@ -24,10 +30,11 @@ const cons = [
   "V",
   "W",
   "X",
+  "Y",
   "Z",
 ];
 
-btn.addEventListener("click", createWord);
+genBtn.addEventListener("click", createWord);
 
 function chooseLetter() {
   let randomNumber = Math.floor(Math.random() * 2);
@@ -54,3 +61,25 @@ function createWord() {
 
   display.textContent = word;
 }
+
+const savedWordsDisplay = document.querySelector(".display_words");
+
+function saveWord() {
+  const savedWord = display.textContent;
+
+  const savedWordInList = document.createElement("li");
+  const savedWordWord = document.createElement("p");
+  savedWordInList.appendChild(savedWordWord);
+  savedWordWord.textContent = savedWord;
+  savedWordInList.className = "saved-li";
+
+  const delBtn = document.createElement("button");
+  delBtn.className = "del-btn";
+  delBtn.innerHTML = delIcon;
+  delBtn.dataset.action = "delete";
+
+  savedWordInList.appendChild(delBtn);
+  savedWordsDisplay.appendChild(savedWordInList);
+}
+
+saveBtn.addEventListener("click", saveWord);
